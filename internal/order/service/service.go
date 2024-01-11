0bb0baf4bd048f33702bcb32bc0facbe28c2f31b
vungocthach <vungocthach1112@gmail.com>
2024-01-11 23:30:34 +0700

    change gitignore + init order service

---
+++ internal/order/service/service.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+func InitService() {
+	// Initialize your service here
+	r := gin.Default()
+
+	// Add your routes and middleware here
+
+	// Connect to the database
+	db, err := gorm.Open("mysql", "user:password@tcp(localhost:3306)/database_name?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+	defer db.Close()
+
+	// Start the server
+	r.Run(":8080")
+}
+
+func main() {
+	InitService()
+}
